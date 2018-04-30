a88f3ab24a6f3f17bfd95fb0602854eecf8de83d
Doddy Tri Hutomo <doddytrihutomo@gmail.com>
2018-04-30 19:19:06 +0700

    Initial setup

---
+++ Mini-Tokopedia/App/SwinjectStoryboard+Setup.swift
@@ -0,0 +1,44 @@
+//
+//  SwinjectStoryboard+Setup.swift
+//  Mini-Tokopedia
+//
+//  Created by Doddy Tri Hutomo on 30/04/18.
+//  Copyright © 2018 Doddy. All rights reserved.
+//
+
+import Foundation
+import Swinject
+import SwinjectStoryboard
+
+extension SwinjectStoryboard {
+    
+    class func setup() {
+        registerStoryboards()
+        registerViewModels()
+        registerInteractors()
+        registerAdapters()
+    }
+    
+    class func resolveDefault<Service>(_ service: Service.Type) -> Service? {
+        return SwinjectStoryboard.defaultContainer.resolve(service)
+    }
+    
+    //MARK: - Private
+    
+    private class func registerStoryboards() {
+        
+    }
+    
+    private class func registerViewModels() {
+        
+    }
+    
+    private class func registerInteractors() {
+        
+    }
+    
+    private class func registerAdapters() {
+        
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Mini-Tokopedia' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Mini-Tokopedia
+
+pod 'Swinject'
+pod 'SwinjectStoryboard'
+pod 'RxSwift'
+pod 'R.swift'
+pod 'SegueManager'
+pod 'SegueManager/R.swift'
+pod 'IQKeyboardManagerSwift'
+
+  target 'Mini-TokopediaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
