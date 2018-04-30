550711595c3ca28aa10cfc37836d1a2ccc7ae501
Doddy Tri Hutomo <doddytrihutomo@gmail.com>
2018-04-30 18:44:03 +0700

    Initial Commit

---
+++ Mini-Tokopedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini-Tokopedia
+//
+//  Created by Doddy Tri Hutomo on 30/04/18.
+//  Copyright © 2018 Doddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
